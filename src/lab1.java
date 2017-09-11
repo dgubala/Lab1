991913efde5b5b802c300e5515cb2bbc5b500082
Jack OHare <johare@luc.edu>
2017-09-11 13:53:17 +0000

    src/lab1.java

---
+++ src/lab1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class lab1 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = 0;
+
+        System.out.println("Pick a Number!");
+        n = scan.nextInt();
+
+        for (int i=1; i<n + 1; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizz buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
